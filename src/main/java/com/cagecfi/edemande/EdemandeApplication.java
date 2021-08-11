8eab0422749acd3e33284f51206e5989a0ab405d
Cecile NYANOU <cecilenyak@gmail.com>
2021-08-11 08:59:09 +0000

    init

---
+++ src/main/java/com/cagecfi/edemande/EdemandeApplication.java
@@ -0,0 +1,13 @@
+package com.cagecfi.edemande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdemandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdemandeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cagecfi/edemande/EdemandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagecfi.edemande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdemandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
